a54f4e630ba3a1ae07d8c1033bc3d6843e6c5658
Renitier <Mirka0999@mail.ru>
2024-07-06 13:35:35 +0300

    Add files via upload

---
+++ InputHelper.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoList_Namespace
+{
+    class InputHelper // Класс для ограничения ввода
+    {
+        public static bool ValidateInt(char c) //Проверка на ввод числа
+        {
+            return char.IsDigit(c);
+        }
+
+        public static int InputInt(string prompt) // Метод, позволяющий ввести только число
+        {
+
+            string input = "";
+            char c;
+
+            while (true)
+            {
+                c = Console.ReadKey(true).KeyChar;
+                // Если пользователь нажал Enter и строка не пуста, то завершаем ввод
+                if (c == '\r' || c == '\n')
+                {
+                    if (string.IsNullOrEmpty(input))
+                    {
+                        continue; //Перезапуск цикла, чтобы пользователь ввел число
+                    }
+                    break;
+                }
+                // Если нажат пробел, то игнорируем
+                if (c == ' ')
+                {
+                    continue;
+                }
+                // Если нажат Backspace, то удаляем последний символ
+                if (c == '\b')
+                {
+                    if (!string.IsNullOrEmpty(input))
+                    {
+                        input = input.Substring(0, input.Length - 1);
+                        Console.Write("\b \b");
+                    }
+                }
+                else if (!ValidateInt(c)) //Если введенный символ не цифра, то игнорируем
+                {
+                    continue;
+                }
+                else //Иначе выводим введенный символ и добавляем его в строку
+                {
+                    Console.Write(c);
+                    input += c;
+                }
+            }
+            // Если строка пуста, возвращаем 0
+            if (string.IsNullOrEmpty(input))
+            {
+                return 0;
+            }
+
+            Console.WriteLine();
+            return int.Parse(input);
+        }
+    }
+}
--- TodoList.cs
+++ TodoList.cs
@@ -1,78 +1,100 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.Design;
 using System.IO;
-using System.Linq;
+using TodoList_Namespace;
+
+/// <summary>
+/// Переменная для выбора типа фильтрации
+/// </summary>
+enum ViewTasks_choice
+{
+    ViewAllTasks = 1,
+    ViewCompletedTasks = 2,
+    ViewIncompleteTasks = 3
+}
 
 class Task 
 {
-    public string Task_name { get; set; } //Наименование задачи
-    public bool Completed { get; set; }  //Выполнена ли она или нет
+    /// <summary>
+    /// Наименование задачи
+    /// </summary>
+    public string Task_name { get; set; }
+    /// <summary>
+    /// Выполнена задача или нет
+    /// </summary>
+    public bool Completed { get; set; }
 
     public Task(string task_name)
     { Task_name = task_name;
-      Completed = false;   // по умолчанию задача не выполнена
+      Completed = false;
     }
 
 }
 
 class TodoList
-{
+{    /// <summary>
+     /// Список задач
+     /// </summary>
     private List<Task> tasks = new List<Task>();
-
-    public void AddTask(string taskName) // Добавление новой задачи в список
+    /// <summary>
+    /// Добавление новой задачи в список
+    /// </summary>
+    public void AddTask(string taskName)
     {
         Task newTask = new Task(taskName);
         tasks.Add(newTask);
     }
-
+    /// <summary>
+    /// Просмотр задач
+    /// </summary>
     public void ViewTasks() // Просмотр задач
     {
-        int choice;
+        ViewTasks_choice choice;
         while (true)
         {
             Console.WriteLine("Какие задачи вы хотите посмотреть?");
             Console.WriteLine("1. Все");
             Console.WriteLine("2. Только выполненные");
             Console.WriteLine("3. Только невыполненные");
-            choice = InputHelper.InputInt("Введите номер фильтрации");
-            if (choice == 1 || choice == 2 || choice == 3)
+            if (Enum.TryParse(Console.ReadLine(), out choice))
             {
-                if (choice == 1) // Просмотр всех задач
+                switch (choice)
                 {
-                    Console.WriteLine("\nВыполненные задачи отмечены знаком \"X\"");
-                    for (int i = 0; i < tasks.Count; i++)
-                    {
-                        Console.WriteLine($"{i + 1}.{tasks[i].Task_name} [{(tasks[i].Completed ? "X" : " ")}]");
-                    }
-                    break;
-                }
-                else if (choice == 2) // Просмотр всех выполненных задач
-                {
-                    Console.WriteLine("\nВыполненные задачи отмечены знаком \"X\"");
-                    for (int i = 0; i < tasks.Count; i++)
-                    {
-                        if (tasks[i].Completed == true)
+                    case ViewTasks_choice.ViewAllTasks:
+                        Console.WriteLine("\nВыполненные задачи отмечены знаком \"X\"");
+                        for (int i = 0; i < tasks.Count; i++)
                         {
                             Console.WriteLine($"{i + 1}.{tasks[i].Task_name} [{(tasks[i].Completed ? "X" : " ")}]");
                         }
-                        else continue;
-                    }
-                    break;
-                }
-                else // Просмотр всех невыполненных задач
-                {
-                    Console.WriteLine("\nВыполненные задачи отмечены знаком \"X\"");
-                    for (int i = 0; i < tasks.Count; i++)
-                    {
-                        if (tasks[i].Completed == false)
+                        break;
+                    case ViewTasks_choice.ViewCompletedTasks:
+                        Console.WriteLine("\nВыполненные задачи отмечены знаком \"X\"");
+                        for (int i = 0; i < tasks.Count; i++)
                         {
-                            Console.WriteLine($"{i + 1}.{tasks[i].Task_name} [{(tasks[i].Completed ? "X" : " ")}]");
+                            if (tasks[i].Completed == true)
+                            {
+                                Console.WriteLine($"{i + 1}.{tasks[i].Task_name} [{(tasks[i].Completed ? "X" : " ")}]");
+                            }
+                            else continue;
                         }
-                        else continue;
-                    }
-                    break;
+                        break;
+                    case ViewTasks_choice.ViewIncompleteTasks:
+                        Console.WriteLine("\nВыполненные задачи отмечены знаком \"X\"");
+                        for (int i = 0; i < tasks.Count; i++)
+                        {
+                            if (tasks[i].Completed == false)
+                            {
+                                Console.WriteLine($"{i + 1}.{tasks[i].Task_name} [{(tasks[i].Completed ? "X" : " ")}]");
+                            }
+                            else continue;
+                        }
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Неверный номер команды");
+                        break;
                 }
+                break;
             }
             else
             {
@@ -81,9 +103,10 @@ class TodoList
             }
         }
     }
-
-
-    public void MarkTaskAsCompleted(int index) // Отметка задачи как выполненной
+    /// <summary>
+    /// Отметка задач как выполненных
+    /// </summary>
+    public void MarkTaskAsCompleted(int index)
     {
         if (index >= 0 && index < tasks.Count)
         {
@@ -94,8 +117,10 @@ class TodoList
             Console.WriteLine("Неверный номер задачи");
         }
     }
-
-    public void DeleteTask(int index) // удаление задачи
+    /// <summary>
+    /// Удаление задачи
+    /// </summary>
+    public void DeleteTask(int index)
     {
         if (index >=0 && index < tasks.Count)
         {
@@ -107,14 +132,18 @@ class TodoList
             Console.WriteLine("Неверный номер задачи");
         }
     }
-
-    public void SaveTodoListToFile(string fileName) //Сохранение списка в файл
+    /// <summary>
+    /// Сохранение списка в файл
+    /// </summary>
+    public void SaveTodoListToFile(string fileName)
     {
         List<string> lines = tasks.Select(task => $"{task.Task_name},{task.Completed}").ToList();
         File.WriteAllLines(fileName, lines);
     }
-
-    public void LoadTodoListFromFile(string fileName) //Загрузка списка из файла
+    /// <summary>
+    /// Загрузка списка в файл
+    /// </summary>
+    public void LoadTodoListFromFile(string fileName)
     {
         if (File.Exists(fileName))
         {
@@ -131,65 +160,7 @@ class TodoList
         }
     }
 }
-class InputHelper // Класс для ограничения ввода
-{
-    public static bool ValidateInt(char c) //Проверка на ввод числа
-    {
-        return char.IsDigit(c);
-    }
 
-    public static int InputInt(string prompt) // Метод, позволяющий ввести только число
-    {
-
-        string input = "";
-        char c;
-
-        while (true)
-        {
-            c = Console.ReadKey(true).KeyChar;
-            // Если пользователь нажал Enter и строка не пуста, то завершаем ввод
-            if (c == '\r' || c == '\n')
-            {
-                if (string.IsNullOrEmpty(input))
-                {
-                    continue; //Перезапуск цикла, чтобы пользователь ввел число
-                }
-                break;
-            }
-            // Если нажат пробел, то игнорируем
-            if (c == ' ')
-            {
-                continue;
-            }
-            // Если нажат Backspace, то удаляем последний символ
-            if (c == '\b')
-            {
-                if (!string.IsNullOrEmpty(input))
-                {
-                    input = input.Substring(0, input.Length - 1);
-                    Console.Write("\b \b");
-                }
-            }
-            else if (!ValidateInt(c)) //Если введенный символ не цифра, то игнорируем
-            {
-                continue;
-            }
-            else //Иначе выводим введенный символ и добавляем его в строку
-            {
-                Console.Write(c);
-                input += c;
-            }
-        }
-        // Если строка пуста, возвращаем 0
-        if (string.IsNullOrEmpty(input))
-        {
-            return 0;
-        }
-
-        Console.WriteLine();
-        return int.Parse(input);
-    }
-}
 internal class Program
 {
     private static void Main()
